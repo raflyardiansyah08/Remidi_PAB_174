eabcec2be502420f649bd80c64d06deff19f237d
RaflyArdiansyah <ardisrigala18@gmail.com>
2026-06-21 21:18:25 +0700

    Deploy Flutter Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2427120505" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1756456856" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
